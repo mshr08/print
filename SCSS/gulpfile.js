412a2365059d2b13102c514e3efab199b066d612
Harshini <maadasriharshini@gmail.com>
2024-11-05 18:57:38 +0530

    Day-1

---
+++ SCSS/gulpfile.js
@@ -0,0 +1,15 @@
+const {src, dest, watch, series} = require('gulp')
+const sass  = require('gulp-sass')(require('sass'))
+
+function buildStyles(){
+    return src('index.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+n
+function watchTask(){
+watch(['index.scss'], buildStyles)
+
+}
+
+exports.default = series(buildStyles, watchTask)
